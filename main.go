95ee8874da03157a0a3e70da9a56583d0681e261
yangdongxu <yangdongxu@kingsoft.com>
2023-04-25 16:09:14 +0800

    update: 在sdk请求的user-agent里增加packer标记

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/KscSDK/ksc-sdk-go/ksc"
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"github.com/kingsoftcloud/packer-plugin-ksyun/builder/epc"
 	"github.com/kingsoftcloud/packer-plugin-ksyun/builder/kec"
 	"os"
 )
 
+var (
+	version string
+)
+
 func main() {
+
+	ksc.SDKName = "packer-plugin-ksyun"
+	ksc.SDKVersion = version
+
 	pps := plugin.NewSet()
 	pps.RegisterBuilder("kec", new(kec.Builder))
 	pps.RegisterBuilder("epc", new(epc.Builder))
